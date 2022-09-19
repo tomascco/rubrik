9f6adc65a702702b8ffc6ea45cc85b9430992044
Tomás Coêlho <tomascoelho6@gmail.com>
2022-09-19 12:32:31 -0300

    Finish Sign and add test files

--- rubrik/sign.rb
+++ rubrik/sign.rb
@@ -16,7 +16,7 @@ module Rubrik
       io.gets("/Contents ")
 
       first_length_in_bytes = io.pos
-      second_byte_range_offset = io.pos + SIGNATURE_SIZE
+      second_byte_range_offset = first_length_in_bytes + SIGNATURE_SIZE
       second_length_in_bytes = io.size - second_byte_range_offset
 
       byte_range_array = [0, first_length_in_bytes, second_byte_range_offset, second_length_in_bytes]
@@ -28,28 +28,27 @@ module Rubrik
       io.seek(-byte_array_size, IO::SEEK_CUR)
       io.write(actual_byte_range)
 
-      chunks_to_digest = Digest::SHA256.new
-
       io.pos = 0
-      chunks_to_digest << T.must(io.read(first_length_in_bytes))
+      data_to_sign = T.must(io.read(first_length_in_bytes))
 
       io.pos = second_byte_range_offset
-      chunks_to_digest << T.must(io.read(second_length_in_bytes))
+      data_to_sign += T.must(io.read(second_length_in_bytes))
 
-      cert_file = File.open("/home/tomas/trabalho/pdf_experiment/certificate-dev.pem")
+      cert_file = File.read("utils/demo_cert.pem")
       cert = OpenSSL::X509::Certificate.new(cert_file)
+      pkey = OpenSSL::PKey::RSA.new(cert_file, "1234")
 
-      cert_file.rewind
-      pkey = OpenSSL::PKey::RSA.new(cert_file)
+      signature = OpenSSL::PKCS7.sign(cert, pkey, data_to_sign, [],
+                                      OpenSSL::PKCS7::DETACHED | OpenSSL::PKCS7::BINARY).to_der
 
-      signature = OpenSSL::PKCS7.sign(cert, pkey, chunks_to_digest.digest, [], OpenSSL::PKCS7::BINARY).to_der
+      hex_signature = T.let(signature, String).unpack1("H*")
+
+      padded_contents_field = "<#{hex_signature.ljust(SIGNATURE_SIZE - 2, "0")}>"
 
       io.pos = first_length_in_bytes
-      io.write("<#{T.let(signature, String).unpack("H*").join.upcase}>")
-
+      io.write(padded_contents_field)
     ensure
       T.must(io).close
-      T.must(cert_file).close
     end
   end
 end
