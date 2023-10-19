fd88a88889aca89dd45e98609a5bf922887a0d10
Tomás Coêlho <tomascoelho6@gmail.com>
2023-10-19 18:49:34 +0000

    Add "integration" tests

--- test/rubrik/sign_test.rb
+++ test/rubrik/sign_test.rb
@@ -5,8 +5,9 @@ require "test_helper"
 
 module Rubrik
   class SignTest < Rubrik::Test
+    def test_document_with_interactive_form
       # Arrange
-      input_pdf = File.open("test/support/simple.pdf", "rb")
+      input_pdf = File.open(SupportPDF["with_interactive_form"], "rb")
       output_pdf = StringIO.new
       certificate = File.open("test/support/demo_cert.pem", "rb")
 
@@ -18,11 +19,62 @@ module Rubrik
       Sign.call(input_pdf, output_pdf, private_key:, public_key:)
 
       # Assert
-      output_pdf.rewind
+      expected_output = File.open(SupportPDF["with_interactive_form.expected"], "rb")
 
-      output_pdf.close
-      input_pdf.close
-      certificate.close
+      expected_output.readlines.zip(output_pdf.readlines).each do |(expected_line, actual_line)|
+        # We must erase the signature because it is timestampped
+        if T.must(actual_line).match?("/Type /Sig")
+          T.must(actual_line).sub!(/<[a-f0-9]+>/, "")
+          T.must(expected_line).sub!(/<[a-f0-9]+>/, "")
+        # The signature field name is also random
+        elsif T.must(actual_line).match?("Signature-[a-f0-9]{4}")
+          T.must(actual_line).sub!(/Signature-[a-f0-9]{4}/, "")
+          T.must(expected_line).sub!(/Signature-[a-f0-9]{4}/, "")
+        end
+
+        assert_equal(expected_line, actual_line)
+        end
+    ensure
+      certificate&.close
+      output_pdf&.close
+      input_pdf&.close
+      expected_output&.close
+    end
+
+    def test_document_without_interactive_form
+      # Arrange
+      input_pdf = File.open(SupportPDF["without_interactive_form"], "rb")
+      output_pdf = StringIO.new
+      certificate = File.open("test/support/demo_cert.pem", "rb")
+
+      private_key = OpenSSL::PKey::RSA.new(certificate, "")
+      certificate.rewind
+      public_key = OpenSSL::X509::Certificate.new(certificate)
+
+      # Act
+      Sign.call(input_pdf, output_pdf, private_key:, public_key:)
+
+      # Assert
+      expected_output = File.open(SupportPDF["without_interactive_form.expected"], "rb")
+
+      expected_output.readlines.zip(output_pdf.readlines).each do |(expected_line, actual_line)|
+        # We can't verify the signature because it changes on every run
+        if T.must(actual_line).match?("/Type /Sig")
+          T.must(actual_line).sub!(/<[a-f0-9]+>/, "")
+          T.must(expected_line).sub!(/<[a-f0-9]+>/, "")
+        # The signature field name is also random
+        elsif T.must(actual_line).match?("Signature-[a-f0-9]{4}")
+          T.must(actual_line).sub!(/Signature-[a-f0-9]{4}/, "")
+          T.must(expected_line).sub!(/Signature-[a-f0-9]{4}/, "")
+        end
+
+        assert_equal(expected_line, actual_line)
+      end
+    ensure
+      certificate&.close
+      output_pdf&.close
+      input_pdf&.close
+      expected_output&.close
     end
   end
 end
